ef24b383380b9db64190bcbc4a940d3147317508
MaryKnyazeva <144832306+MaryKnyazeva@users.noreply.github.com>
2023-12-06 11:36:29 +0300

    Add files via upload

---
+++ card.js
@@ -0,0 +1,17 @@
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.card1'), 6, true);
+
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.card2'), 6, true);
